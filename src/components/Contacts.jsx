b8285569e2de59b8bb20f42c70c895427ce9a434
5v374 <sveta04ne@icloud.com>
2024-09-01 18:38:51 +0300

    added vedeo works and contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,9 +4,16 @@ const Contacts = () => {
   return (
     <section id="contacts">
       <h2>Контакты:</h2>
-      <p>телеграмм: <a href="https://t.me/Tatiana_withlove" target="_blank" rel="noopener noreferrer">@Tatiana_withlove</a></p>
+      <address>
+        Телеграмм: <a href="https://t.me/Tatiana_withlove" target="_blank" rel="noopener noreferrer">@Tatiana_withlove</a>
+      </address>
+      <address>Почта: tatyana01ne@mail.ru</address>
+      <address>WhatsApp: 
+        <a href='https://wa.me/qr/LKRTFGX6ZHRYE1'>Татьяна</a>
+      </address>
     </section>
   );
 };
 
 export default Contacts;
+
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const importAll = (r) => r.keys().map(r);
-const images = importAll(require.context('../assets/photos', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(
+  require.context("../assets/photos", false, /\.(png|jpe?g|svg)$/)
+);
 
 const Work = () => {
   const [isVideoOpen, setIsVideoOpen] = useState(false);
@@ -9,9 +11,9 @@ const Work = () => {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
 
   const toggleSection = (section) => {
-    if (section === 'video') {
+    if (section === "video") {
       setIsVideoOpen(!isVideoOpen);
-    } else if (section === 'photo') {
+    } else if (section === "photo") {
       setIsPhotoOpen(!isPhotoOpen);
     }
   };
@@ -27,17 +29,23 @@ const Work = () => {
   return (
     <section id="work">
       <h3>Мои работы:</h3>
-      <h4 onClick={() => toggleSection('video')} style={{ cursor: 'pointer' }}>
-        Видео-работы {isVideoOpen ? '-' : '+'}
+      <h4 onClick={() => toggleSection("video")} style={{ cursor: "pointer" }}>
+        Видео-работы {isVideoOpen ? "-" : "+"}
       </h4>
-      
-      <h4 onClick={() => toggleSection('photo')} style={{ cursor: 'pointer' }}>
-        Фото-работы {isPhotoOpen ? '-' : '+'}
+      {isVideoOpen && (
+        <div>
+          <a href="https://disk.yandex.ru/i/oG1b7eciIT8oEw">Саша-видео</a>
+          <br />
+          <a href="https://disk.yandex.ru/d/6s1ItvGQlB6Xfg">Вика-видео</a>
+        </div>
+      )}
+      <h4 onClick={() => toggleSection("photo")} style={{ cursor: "pointer" }}>
+        Фото-работы {isPhotoOpen ? "-" : "+"}
       </h4>
       {isPhotoOpen && (
         <div className="photo-grid">
           {images.map((photo, index) => (
-            <img 
+            <img
               key={index}
               src={photo}
               alt={`Фото работы ${index + 1}`}
@@ -49,7 +57,11 @@ const Work = () => {
       )}
       {selectedPhoto && (
         <div className="photo-overlay" onClick={closePhoto}>
-          <img src={selectedPhoto} alt="Увеличенное фото" className="image-large" />
+          <img
+            src={selectedPhoto}
+            alt="Увеличенное фото"
+            className="image-large"
+          />
         </div>
       )}
     </section>
