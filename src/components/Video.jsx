26eb7089ff0b6ccbd88b4daa2fc1c10011a21af4
5v374 <sveta04ne@icloud.com>
2025-04-30 17:52:37 +0300

    added a video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
 const Video = () => {
-    return (
-        <section id="video">
-            <h4>Видео-работы</h4>
-            <div className="video">
-              <iframe
-                src="https://rutube.ru/play/embed/d4224087d2e5491e4be4cf6539572d8d"
-                frameBorder="0"
-                allow="autoplay; encrypted-media"
-                allowFullScreen
-                title="Rutube Video"
-              />
-            </div>
-            </section>
-    );
+  return (
+    <section id="video">
+      <h4>Видео-работы</h4>
+
+      <div className="video">
+        {/* Rutube Video */}
+        <iframe
+          src="https://rutube.ru/play/embed/d4224087d2e5491e4be4cf6539572d8d"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="Rutube Video"
+        ></iframe>
+      </div>
+
+      <div className="video">
+        {/* VK Video */}
+        <iframe
+          src="https://vk.com/video_ext.php?oid=-230081631&id=456239017&hash=ab469f7115ef3372"
+          frameBorder="0"
+          allow="autoplay; encrypted-media; fullscreen"
+          allowFullScreen
+          title="VK Video"
+        ></iframe>
+      </div>
+    </section>
+  );
 };
 
 export default Video;
