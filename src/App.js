c22cb696916a0758b9e388a5ddd92771f7a807f3
5v374 <sveta04ne@icloud.com>
2025-05-11 20:04:09 +0300

    download video via json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Video from './components/Video';
+import Video from './components/Video.jsx';
 import Work from './components/Work';
 import Contacts from './components/Contacts';
 
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,30 +1,23 @@
 import React from "react";
+import video from './video.json';
 
 const Video = () => {
   return (
     <section id="video">
       <h4>Видео-работы</h4>
 
-      <div className="video">
-        {/* Rutube Video */}
-        <iframe
-          src="https://rutube.ru/play/embed/d4224087d2e5491e4be4cf6539572d8d"
-          frameBorder="0"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-          title="Rutube Video"
-        ></iframe>
-      </div>
-
-      <div className="video">
-        {/* VK Video */}
-        <iframe
-          src="https://vk.com/video_ext.php?oid=-230081631&id=456239017&hash=ab469f7115ef3372"
-          frameBorder="0"
-          allow="autoplay; encrypted-media; fullscreen"
-          allowFullScreen
-          title="VK Video"
-        ></iframe>
+      <div className="video-list">
+        {video.map((video, index) => (
+          <div key={index} className="video-item" style={{ marginBottom: '20px' }}>
+            <iframe
+              src={video.src}
+              title={video.title}
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
       </div>
     </section>
   );
