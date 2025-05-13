27ec52f99fe4ca458b295f19f5be912dd57117fa
5v374 <sveta04ne@icloud.com>
2025-05-13 20:13:22 +0300

    download photo via json file

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,28 +1,12 @@
 import React, { useState } from "react";
 import "./Work.css";
-
-const portraitImages = [
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745252480/IMG_20250418_141001_201_rykzws.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745252502/IMG_20250418_141000_799_pormor.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745253042/photo6_w0ggwg.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745422988/IMG_20250418_141047_873_mr7aaz.jpg"
-]
-const natureImages = [
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745238437/IMG_20250418_141112_879_fsfhnz.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745253132/photo4_s0gpon.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745253120/photo3_kuo9l9.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745253554/photo8_i4helv.jpg"
-]
-const eventImages = [
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745238426/IMG_20250418_141131_810_qkohq9.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745238418/IMG_20250418_141130_822_mrx3x1.jpg",
-  "https://res.cloudinary.com/dofxtaas7/image/upload/v1745238409/IMG_20250418_141131_696_expcsj.jpg"
-]
+import photoData from './photos.json';
 
 const Work = () => {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [isVisible, setIsVisible] = useState(false);
 
+  const {portraitImages, natureImages, eventImages} = photoData;
   const openPhoto = (photo) => {
     setSelectedPhoto(photo);
   };
